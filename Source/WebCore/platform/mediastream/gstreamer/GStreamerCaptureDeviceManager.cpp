a81e1aac6ec33dca67a6ffadaed8edfdf582ccae
Alejandro G. Castro <alex@igalia.com>
2018-04-12 16:35:46 +0200

    Enumerate proposed patch

---
+++ Source/WebCore/platform/mediastream/gstreamer/GStreamerCaptureDeviceManager.cpp
@@ -0,0 +1,142 @@
+/*
+ *  Copyright (C) 2017 Igalia S.L. All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+
+#if ENABLE(MEDIA_STREAM) && USE(GSTREAMER)
+#include "GStreamerCaptureDeviceManager.h"
+#include "GStreamerUtilities.h"
+
+#include <wtf/glib/GUniquePtr.h>
+
+namespace WebCore {
+
+GStreamerAudioCaptureDeviceManager& GStreamerAudioCaptureDeviceManager::singleton()
+{
+    static NeverDestroyed<GStreamerAudioCaptureDeviceManager> manager;
+    return manager;
+}
+
+GStreamerVideoCaptureDeviceManager& GStreamerVideoCaptureDeviceManager::singleton()
+{
+    static NeverDestroyed<GStreamerVideoCaptureDeviceManager> manager;
+    return manager;
+}
+
+GStreamerDisplayCaptureDeviceManager& GStreamerDisplayCaptureDeviceManager::singleton()
+{
+    static NeverDestroyed<GStreamerDisplayCaptureDeviceManager> manager;
+    return manager;
+}
+
+std::optional<GStreamerCaptureDevice> GStreamerCaptureDeviceManager::gstreamerDeviceWithUID(const String& deviceID)
+{
+    initializeGStreamer();
+    if (m_gstreamerDevices.isEmpty())
+        refreshCaptureDevices();
+
+    for (auto& device : m_gstreamerDevices) {
+        if (device.persistentId() == deviceID)
+            return device;
+    }
+    return std::nullopt;
+}
+
+Vector<CaptureDevice>& GStreamerCaptureDeviceManager::captureDevices()
+{
+    initializeGStreamer();
+    if (m_devices.isEmpty())
+        refreshCaptureDevices();
+
+    return m_devices;
+}
+
+void GStreamerCaptureDeviceManager::deviceAdded(GstDevice* device)
+{
+    GstStructure* properties = gst_device_get_properties(device);
+    const gchar* klass = gst_structure_get_string(properties, "device.class");
+
+    if (klass && !g_strcmp0(klass, "monitor")) {
+        gst_structure_free(properties);
+        return;
+    }
+    gst_structure_free(properties);
+
+    CaptureDevice::DeviceType type = deviceType();
+    GUniquePtr<gchar> deviceClassChar(gst_device_get_device_class(device));
+    String deviceClass(String(deviceClassChar.get()));
+    if (type == CaptureDevice::DeviceType::Microphone && !deviceClass.startsWith("Audio"))
+        return;
+    if (type == CaptureDevice::DeviceType::Camera && !deviceClass.startsWith("Video"))
+        return;
+
+    // FIXME: This isn't really a UID but should be good enough (libwebrtc itself does that
+    // at least for pulseaudio devices.)
+    GUniquePtr<gchar> deviceName(gst_device_get_display_name(device));
+    String identifier = String::fromUTF8(deviceName.get());
+
+    auto gstCaptureDevice = GStreamerCaptureDevice(device, identifier, type, identifier);
+    gstCaptureDevice.setEnabled(true);
+    m_gstreamerDevices.append(WTFMove(gstCaptureDevice));
+    auto captureDevice = CaptureDevice(identifier, type, identifier);
+    captureDevice.setEnabled(true);
+    m_devices.append(WTFMove(captureDevice));
+}
+
+void GStreamerCaptureDeviceManager::refreshCaptureDevices()
+{
+    if (!m_deviceMonitor) {
+        m_deviceMonitor = adoptGRef(gst_device_monitor_new());
+
+        CaptureDevice::DeviceType type = deviceType();
+        if (type == CaptureDevice::DeviceType::Camera) {
+            GRefPtr<GstCaps> caps = adoptGRef(gst_caps_new_empty_simple("video/x-raw"));
+            gst_device_monitor_add_filter(m_deviceMonitor.get(), "Video/Source", caps.get());
+            GRefPtr<GstCaps> compressedCaps = adoptGRef(gst_caps_new_empty_simple("video/x-h264"));
+            gst_device_monitor_add_filter(m_deviceMonitor.get(), "Video/Source", compressedCaps.get());
+        } else if (type == CaptureDevice::DeviceType::Microphone) {
+            GRefPtr<GstCaps> caps = adoptGRef(gst_caps_new_empty_simple("audio/x-raw"));
+            gst_device_monitor_add_filter(m_deviceMonitor.get(), "Audio/Source", caps.get());
+        }
+
+        // TODO: Monitor for added/removed messages on the bus.
+    }
+
+    if (!gst_device_monitor_start(m_deviceMonitor.get())) {
+        GST_WARNING_OBJECT(m_deviceMonitor.get(), "Could not start device monitor");
+        m_deviceMonitor = nullptr;
+
+        return;
+    }
+
+    GList* devices = gst_device_monitor_get_devices(m_deviceMonitor.get());
+    if (devices != NULL) {
+        while (devices != NULL) {
+            GstDevice* device = GST_DEVICE_CAST(devices->data);
+            deviceAdded(device);
+            gst_object_unref(device);
+            devices = g_list_delete_link(devices, devices);
+        }
+    }
+
+    gst_device_monitor_stop(m_deviceMonitor.get());
+}
+
+} // namespace WebCore
+
+#endif // ENABLE(MEDIA_STREAM) && USE(GSTREAMER)
---
+++ Source/WebCore/platform/mediastream/gstreamer/GStreamerVideoCaptureSource.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2017 Igalia S.L. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if ENABLE(MEDIA_STREAM) && USE(LIBWEBRTC)
+
+#include "CaptureDevice.h"
+#include "RealtimeMediaSource.h"
+#include <LibWebRTCMacros.h>
+#include <wtf/HashMap.h>
+#include <wtf/Lock.h>
+#include <wtf/RefCounted.h>
+#include <wtf/RefPtr.h>
+#include <wtf/text/WTFString.h>
+
+#include "webrtc/api/mediastreaminterface.h"
+#include "webrtc/media/base/videocapturer.h"
+#include "webrtc/api/videosinkinterface.h"
+
+#include "gstreamer/GStreamerVideoCapturer.h"
+
+namespace WTF {
+class MediaTime;
+}
+
+namespace WebCore {
+
+class GStreamerVideoCaptureSource : public RealtimeMediaSource {
+public:
+    static CaptureSourceOrError create(const String& deviceID, const MediaConstraints*);
+    GstElement *Pipeline() { return m_capturer->m_pipeline.get(); }
+    GStreamerCapturer *Capturer() { return m_capturer.get(); }
+
+    void startProducingData() override;
+    void stopProducingData() override;
+
+protected:
+    GStreamerVideoCaptureSource(const String& deviceID, const String& name, const gchar * source_factory);
+    virtual ~GStreamerVideoCaptureSource();
+
+    const RealtimeMediaSourceSettings& settings() const override;
+    const RealtimeMediaSourceCapabilities& capabilities() const;
+
+    mutable std::optional<RealtimeMediaSourceCapabilities> m_capabilities;
+    mutable std::optional<RealtimeMediaSourceSettings> m_currentSettings;
+    std::unique_ptr<GStreamerVideoCapturer> m_capturer;
+
+private:
+    GStreamerVideoCaptureSource(GStreamerCaptureDevice device);
+
+    friend class GStreamerVideoCaptureSourceFactory;
+
+    static GstFlowReturn newSampleCallback(GstElement*, GStreamerVideoCaptureSource*);
+
+    bool isCaptureSource() const final { return true; }
+    bool applySize(const IntSize&) final;
+    bool applyFrameRate(double) final;
+    bool applyAspectRatio(double) final { return true; }
+
+    rtc::scoped_refptr<webrtc::VideoTrackInterface> m_videoTrack;
+};
+
+} // namespace WebCore
+
+#endif // ENABLE(MEDIA_STREAM) && USE(LIBWEBRTC)
